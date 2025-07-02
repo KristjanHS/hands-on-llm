d7f0db02a3caffc0cac278addeb93ed8817e3c0c
Kristjan Sillmann <kristjan.h.s@gmail.com>
2025-07-02 12:26:49 +0300

    batch files and wsl config

--- phase1/batch_and_shell/windows_ollama_for_WSL.bat
+++ phase1/batch_and_shell/windows_ollama_for_WSL.bat
@@ -1,3 +1,13 @@
 @echo off
+set OMP_NUM_THREADS=8
+set MKL_NUM_THREADS=8
+
+set OLLAMA_NUM_PARALLEL=1
+set OLLAMA_NUM_THREADS=8  # aga tundub et seda ignoreeritakse
+set OLLAMA_KV_CACHE_TYPE=heap
+set OLLAMA_CONTEXT_LENGTH=8192
+set OLLAMA_HOST=0.0.0.0
+set OLLAMA_NUM_THREADS=8  
+set OLLAMA_CONTEXT_LENGTH=8192
 set OLLAMA_HOST=0.0.0.0
 ollama serve
